6381e22a505a6273f3ca3115b0349671753cd083
Sheishori <sheishori@gmail.com>
2022-02-25 12:30:45 +0100

    Create basic math functions

---
+++ scripts.js
@@ -0,0 +1,18 @@
+function add(number) {
+	return current = current + number;
+}
+
+function subtract(number) {
+	return current = current - number;
+}
+
+function multiply(number) {
+	return current = current * number;
+}
+
+function divide(number) {
+	return current = current / number;
+}
+
+let current = 0;
+console.log(current);
