fcf500a747ce9a78b528f3bf9128c2664da0dde7
Sheishori <sheishori@gmail.com>
2022-02-25 13:35:46 +0100

    Populate the display when pressing number buttons

--- scripts.js
+++ scripts.js
@@ -1,3 +1,19 @@
+const calculator = document.querySelector("#calculator");
+const display = calculator.querySelector("#display");
+const buttons = calculator.querySelector("#buttons");
+const numberButtons = buttons.querySelectorAll(".number");
+numberButtons.forEach((button) => {
+	button.addEventListener("click", (e) => {
+		if (current === 0) {
+			current = e.target.textContent;
+			display.textContent = current;
+		} else {
+			current += e.target.textContent;
+			display.textContent = current;
+		}
+	})
+});
+
 function add(a, b) {
 	return a + b;
 }
@@ -28,3 +44,4 @@ function operate(operator, number) {
 }
 
 let current = 0;
+display.textContent = current;
